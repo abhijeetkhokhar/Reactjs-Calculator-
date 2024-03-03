8bc5b8916b051b26514647b48a796b88cf3edb65
abhijeetkhokhar <abhijeetkhokhar01@gmail.com>
2024-03-03 15:35:26 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import ParentContainer from './components/ParentContainer';
+import Display from './components/Display';
+import Button from './components/Button';
+import React,{ useState } from 'react';
 
-function App() {
+
+export default function App() {
+  const [data,setData]=useState('');
+  function handleClick(val){
+    if(val==='='){
+      setData(eval(data));
+    }else if(val==='C'){
+      setData("");
+    }else if(val==='X'){
+      let res = data+'';
+      setData(res.slice(0,-1));
+    }else{
+      setData(data+val);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ParentContainer>
+      <Display data={data}></Display>
+      <Button btnClick={handleClick}></Button>
+    </ParentContainer>
   );
 }
-
-export default App;
